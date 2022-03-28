d092b88b5cfc1027c79eceee9fb09563da3b8f25
雄飞 张 <1176914912@qq.com>
2022-03-28 18:04:01 +0800

    修改

--- gitbook/gitbook-plugin-sharing/buttons.js
+++ gitbook/gitbook-plugin-sharing/buttons.js
@@ -52,39 +52,39 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
 
 
 
-    gitbook.events.bind('start', function(e, config) {
-        var opts = config.sharing;
+    //gitbook.events.bind('start', function(e, config) {
+    //    var opts = config.sharing;
 
-        // Create dropdown menu
-        //var menu = $.map(opts.all, function(id) {
-        //    var site = SITES[id];
+    //    // Create dropdown menu
+    //    var menu = $.map(opts.all, function(id) {
+    //        var site = SITES[id];
 
-        //    return {
-        //        text: site.label,
-        //        onClick: site.onClick
-        //    };
-        //});
+    //        return {
+    //            text: site.label,
+    //            onClick: site.onClick
+    //        };
+    //    });
 
-        // Create main button with dropdown
-        if (menu.length > 0) {
-            gitbook.toolbar.createButton({
-                icon: 'fa fa-share-alt',
-                label: 'Share',
-                position: 'right',
-                dropdown: [menu]
-            });
-        }
+    //    // Create main button with dropdown
+    //    if (menu.length > 0) {
+    //        gitbook.toolbar.createButton({
+    //            icon: 'fa fa-share-alt',
+    //            label: 'Share',
+    //            position: 'right',
+    //            dropdown: [menu]
+    //        });
+    //    }
 
-        // Direct actions to share
-        $.each(SITES, function(sideId, site) {
-            if (!opts[sideId]) return;
+    //    // Direct actions to share
+    //    $.each(SITES, function(sideId, site) {
+    //        if (!opts[sideId]) return;
 
-            gitbook.toolbar.createButton({
-                icon: site.icon,
-                label: site.text,
-                position: 'right',
-                onClick: site.onClick
-            });
-        });
-    });
+    //        gitbook.toolbar.createButton({
+    //            icon: site.icon,
+    //            label: site.text,
+    //            position: 'right',
+    //            onClick: site.onClick
+    //        });
+    //    });
+    //});
 });
